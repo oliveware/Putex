367cfe161994d2213f40c5e3b1df27e9872c3a18
herve <crespel.herve@wanadoo.fr>
2025-02-17 12:26:08 +0100

    let origine

--- Sources/Putex/durée/JMA.swift
+++ Sources/Putex/durée/JMA.swift
@@ -20,7 +20,7 @@ func mois(nomfr:String)->Int {
 public struct JMA: Codable, Equatable {
     public static let moisfr = ["janvier","février","mars","avril","mai","juin",
         "juillet","août","septembre","octobre","novembre","décembre"]
-    public static var origine = JMA(1,1,2000)
+    public static let origine = JMA(1,1,2000)
     
     public static func == (_ lhs: JMA,_ rhs: JMA) -> Bool {
         return lhs.jour == rhs.jour && lhs.mois == rhs.mois && lhs.année == rhs.année
