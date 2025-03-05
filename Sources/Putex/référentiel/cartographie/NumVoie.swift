db10683dfb0735c308e6568b538048f0c74c857b
herve <crespel.herve@wanadoo.fr>
2025-03-05 08:57:08 +0100

    adresse postale

--- Sources/Putex/référentiel/cartographie/NumVoie.swift
+++ Sources/Putex/référentiel/cartographie/NumVoie.swift
@@ -32,3 +32,36 @@ public struct NumVoie: Codable, Equatable {
         codepostal = cp
     }
 }
+
+
+public struct AdressePostale: Codable {
+    var l1:String
+    var l2:String?
+    var l3:String
+    var l4:String
+    var l5:String?
+    
+    public init(l1: String, l3: String, l4: String, l2: String? = nil, l5: String? = nil) {
+        self.l1 = l1
+        self.l2 = l2
+        self.l3 = l3
+        self.l4 = l4
+        self.l5 = l5
+    }
+    
+    var astring: String {
+        var lines = l1 + "\n"
+        if l2 != nil {lines = lines + l2! + "\n"}
+        lines = lines + l3 + "\n"
+        lines = lines + l4
+        if l5 != nil {lines = "\n" + lines + l5!}
+        return lines
+    }
+    
+    init(_ nom:String, _ numvoie:NumVoie, _ complement:String? = nil) {
+        l1 = nom
+        l2 = complement
+        l3 = numvoie.num + " " + numvoie.voie
+        l4 = numvoie.codepostal + "" //Commune[codepostal]
+    }
+}
