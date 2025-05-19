b772cba689b92c0f11b1f2ec4e9910c0cce14471
herve <crespel.herve@wanadoo.fr>
2025-05-19 20:52:48 +0200

    identi

--- Sources/Putex/choix/oxets/protocol Oxet.swift
+++ Sources/Putex/choix/oxets/protocol Oxet.swift
@@ -5,7 +5,7 @@
 //  Created by Herve Crespel on 09/05/2025.
 //
 
-public protocol Oxet: Codable {
+public protocol Oxet: Codable, Identifiable {
     var id:String {get}
     var label:String {get}
     static var selector:String {get}
