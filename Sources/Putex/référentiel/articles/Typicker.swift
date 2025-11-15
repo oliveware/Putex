8eac801c5a078c5d011f476a84d806be2d8658c3
herve <crespel.herve@wanadoo.fr>
2025-11-15 23:32:03 +0100

    corr

--- Sources/Putex/référentiel/articles/Typicker.swift
+++ Sources/Putex/référentiel/articles/Typicker.swift
@@ -9,20 +9,35 @@ import SwiftUI
 public struct Typicker: View {
     @Binding var tid : String
     var types:[TypeArticle]
+    @State var pick : Bool
     
     public init(_ tid:Binding<String>, _ types:[TypeArticle]) {
         _tid = tid
         self.types = types
+        pick = tid.wrappedValue == ""
     }
     
     public var body:some View {
         if types.count > 0 {
-            ForEach(types) {
-                type in
-                HStack {
-                    Text(type.show)
-                    Button(action: {tid = type.id})
-                    { Image(systemName: "pencil") }
+            HStack {
+                if tid == "" {
+                    Text("choisir un type")
+                } else {
+                    Text(tid)
+                }
+                Button(action: {pick = true})
+                { Image(systemName: "pencil") }
+                    .sheet(isPresented:$pick)
+                {ScrollView {
+                    ForEach(types) {
+                        type in
+                        HStack {
+                            Text(type.show)
+                            Button(action: {tid = type.id})
+                            { Image(systemName: "pencil") }
+                        }
+                    }
+                }
                 }
             }
         } else {
