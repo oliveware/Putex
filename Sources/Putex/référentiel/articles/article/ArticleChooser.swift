4285fdbf6b47c33e9ddbca697b2d5685d716348e
herve <crespel.herve@wanadoo.fr>
2025-12-07 23:28:35 +0100

    point

---
+++ Sources/Putex/référentiel/articles/article/ArticleChooser.swift
@@ -0,0 +1,50 @@
+//
+//  ArticleChooser.swift
+//  Putex
+//
+//  Created by Herve Crespel on 07/12/2025.
+//
+
+import SwiftUI
+
+public struct ArticleChooser : View {
+    var ref : Articleref
+    @Binding var article: Article
+    var done: () -> Void
+    
+    public init(_ article:Binding<Article>, _ ref:Articleref, _ done:@escaping () -> Void) {
+        _article = article
+        self.ref = ref
+     //  taxion = ref.besoin(article.wrappedValue.tid)
+        self.done = done
+    }
+    
+    var tidid: String {article.tid}
+    
+    public var body : some View {
+        VStack {
+
+            if ref.type(tidid).id == "" {
+                Text("aucun article de ce type")
+            } else {
+                GroupBox("Articles disponibles") {
+                    ScrollView {
+                        ForEach(ref.articlelist(tidid)){
+                            item in
+                            HStack(spacing:30) {
+                                Text(article.show)
+                                Spacer()
+                                Button(action:{
+                                    article = item
+                                    done()
+                                })
+                                {Image(systemName: "square.and.arrow.down")}
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+}
--- Sources/Putex/référentiel/articles/article/ArticlePicker.swift
+++ Sources/Putex/référentiel/articles/article/ArticlePicker.swift
@@ -5,49 +5,41 @@
 //  Created by Herve Crespel on 19/11/2025.
 //
 import SwiftUI
-import Taxionomy
-import Putex
 
 
-public struct ArticlePicker : View {
+struct ArticlePicker : View {
     var ref : Articleref
     @Binding var article: Article
     var done: () -> Void
+    @State var pick : Bool
     
     public init(_ article:Binding<Article>, _ ref:Articleref, _ done:@escaping () -> Void) {
         _article = article
         self.ref = ref
      //  taxion = ref.besoin(article.wrappedValue.tid)
         self.done = done
+        pick = article.wrappedValue.isNaN
     }
-    
-    var tidid: String {article.tid}
-    
-    public var body : some View {
-        VStack {
 
-            if ref.type(tidid).id == "" {
-                Text("aucun article de ce type")
-            } else {
-                GroupBox("Articles disponibles") {
-                    ScrollView {
-                        ForEach(ref.articlelist(tidid)){
-                            item in
-                            HStack(spacing:30) {
-                                Text(article.show)
-                                Spacer()
-                                Button(action:{
-                                    article = item
-                                    done()
-                                })
-                                {Image(systemName: "square.and.arrow.down")}
-                            }
-                        }
-                    }
-                }
+    
+   var body : some View {
+        VStack (){
+            HStack {
+                Spacer()
+                Text("article").font(.title3)
+                Spacer()
+                Button(action:{pick = true})
+                {Image(systemName: "list.triangle")}
             }
-        }
-        
+            if pick {
+                ArticlePicker($article, ref, {
+                    pick = false
+                    done()
+                })
+            } else {
+                ArticleShow(article, ref)
+            }
+        }.padding().border(.gray).cornerRadius(3)
     }
 }
 
