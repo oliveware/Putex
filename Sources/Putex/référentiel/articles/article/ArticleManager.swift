a14a156b29fa1a3c440dee734f17648a6d0b469e
herve <crespel.herve@wanadoo.fr>
2025-12-18 12:21:43 +0100

    pointest

--- Sources/Putex/référentiel/articles/article/ArticleManager.swift
+++ Sources/Putex/référentiel/articles/article/ArticleManager.swift
@@ -12,29 +12,34 @@ public struct ArticleManager : View {
     @Binding var ref: Articleref
     
     @State var selected = 0
-    @State var full = false
+    @State var full : Bool?
     
-    public init(_ ref:Binding<Articleref>) {
+    public init(_ ref:Binding<Articleref>, _ full:Bool = false) {
         _ref = ref
+        if full {self.full = true}
     }
     
     public var body: some View {
-        TabView(selection: $selected) {
-            ArticleList($ref.articles, ref)
-                .tabItem { Text("articles") }.tag(1)
-            TypeArticleManager($ref.types, ref.besoins)
-                .tabItem { Text("types") }.tag(2)
-            if full {
-                TaxionomyManager($ref.besoins)
-                    .tabItem { Text("besoins") }.tag(3)
-                TaxionomyManager($ref.contenants)
-                    .tabItem { Text("contenants") }.tag(4)
-                TaxionomyManager($ref.fermetures)
-                    .tabItem { Text("fermetures") }.tag(5)
+        HStack(alignment:.top) {
+            TabView(selection: $selected) {
+                ArticleList($ref.articles, ref)
+                    .tabItem { Text("articles") }.tag(1)
+                TypeArticleManager($ref.types, ref.besoins)
+                    .tabItem { Text("types") }.tag(2)
+                if let full = full {
+                    TaxionomyManager($ref.besoins)
+                        .tabItem { Text("besoins") }.tag(3)
+                    TaxionomyManager($ref.contenants)
+                        .tabItem { Text("contenants") }.tag(4)
+                    TaxionomyManager($ref.fermetures)
+                        .tabItem { Text("fermetures") }.tag(5)
+                }
             }
             Spacer()
-            Button(action:{full.toggle()})
-            {Image(systemName: full ? "chevron.left" : "chevron.right")}
+            if let full = full {
+                Button(action:{self.full!.toggle()})
+                {Image(systemName: full ? "chevron.left" : "chevron.right")}
+            }
         
         }
     }
