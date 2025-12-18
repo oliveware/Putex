06c84cf5eeb2a7f6bce9657a4a89532e443a0ab7
herve <crespel.herve@wanadoo.fr>
2025-12-18 12:56:58 +0100

    pointest

--- Sources/Putex/référentiel/articles/article/ArticleManager.swift
+++ Sources/Putex/référentiel/articles/article/ArticleManager.swift
@@ -12,35 +12,49 @@ public struct ArticleManager : View {
     @Binding var ref: Articleref
     
     @State var selected = 0
-    @State var full : Bool?
+    @State var full : Bool
     
-    public init(_ ref:Binding<Articleref>, _ full:Bool = false) {
+    public init(_ ref:Binding<Articleref>, _ full:Bool) {
         _ref = ref
-        if full {self.full = true}
+        self.full = full
     }
     
     public var body: some View {
-        HStack(alignment:.top) {
-            TabView(selection: $selected) {
-                ArticleList($ref.articles, ref)
-                    .tabItem { Text("articles") }.tag(1)
-                TypeArticleManager($ref.types, ref.besoins)
-                    .tabItem { Text("types") }.tag(2)
-                if let full = full {
-                    TaxionomyManager($ref.besoins)
-                        .tabItem { Text("besoins") }.tag(3)
-                    TaxionomyManager($ref.contenants)
-                        .tabItem { Text("contenants") }.tag(4)
-                    TaxionomyManager($ref.fermetures)
-                        .tabItem { Text("fermetures") }.tag(5)
-                }
+        VStack {
+            GroupBox {
+                HStack {
+                    Button("articles", action:{selected = 1})
+                    Button("types", action:{selected = 2})
+                    
+                    if full {
+                        Button("besoins", action:{selected = 3})
+                        Button("contenants", action:{selected = 4})
+                        Button("fermetures", action:{selected = 5})
+                    }
+                    Spacer()
+                    Button(action:{full.toggle()})
+                    {Image(systemName: full ? "chevron.left" : "chevron.right")}
+                    
+                }.padding(3)
             }
             Spacer()
-            if let full = full {
-                Button(action:{self.full!.toggle()})
-                {Image(systemName: full ? "chevron.left" : "chevron.right")}
+            switch selected {
+            case 1 :
+                ArticleList($ref.articles, ref)
+            case 2 :
+                TypeArticleManager($ref.types, ref.besoins)
+            case 3 :
+                TaxionomyManager($ref.besoins)
+            case 4 :
+                TaxionomyManager($ref.contenants)
+            case 5 :
+                TaxionomyManager($ref.fermetures)
+            default : Text("")
             }
-        
         }
     }
+}
+
+#Preview {
+    ArticleManager(.constant(ArticlerefDocument().ref), true).frame(width:800, height:600)
 }
