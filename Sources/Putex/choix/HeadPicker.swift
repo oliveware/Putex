25bbd6166f9dda962b01b8c27d4761a13ebc56bf
herve <crespel.herve@wanadoo.fr>
2026-03-23 14:55:01 +0100

    corr

--- Sources/Putex/choix/HeadPicker.swift
+++ Sources/Putex/choix/HeadPicker.swift
@@ -13,83 +13,88 @@ public struct HeadPicker: View {
     
     var table : Coderef
     @Binding var head : Head?
-    @State var ajout = false
-    @State var open = false
+    @State var input = true
+    @State var choice = false
     
-    /*public init(_ open: Binding<Bool>, _ table:Coderef, _ selected:Binding<Head?>,_ prompt:String?) {
+    /*public init(_ choice: Binding<Bool>, _ table:Coderef, _ selected:Binding<Head?>,_ prompt:String?) {
         self.prompt = prompt ?? "Choisir " + table.name.indéterminé
         self.table = table
         _head = selected
-        _open = open
+        _choice = choice
     }*/
     
-    public init(_ head:Binding<Head?>, _ domain:Codomain) {
-        table = Coderef.find(domain)
+    public init(_ head:Binding<Head?>, _ domain:Codomain, _ prompt:String?) {
+        let table = Coderef.find(domain)
+        self.prompt = prompt ?? table.name.singulier
+        self.table = table
         _head = head
     }
-    public init(_ head:Binding<Head?>, _ ref:Coderef) {
-        table = ref
+    public init(_ head:Binding<Head?>, _ ref:Coderef, _ prompt:String?) {
+        let table = ref
+        self.prompt = prompt ?? table.name.singulier
+        self.table = table
         _head = head
     }
     
     public var body: some View {
-        if open {
-            VStack {
-                Text(prompt)
-                ScrollView {
-                    VStack(spacing:2){
-                        ForEach(table.items) {
-                            head in
-                            Button( action: {
-                                choose(head)
-                            }){
-                                Text(head.label)
+        HStack {
+            
+            if choice {
+                GroupBox("choisir " +  prompt) {
+                    ScrollView {
+                        VStack(spacing:2){
+                            ForEach(table.items) {
+                                head in
+                                Button( action: {
+                                    choose(head)
+                                }){
+                                    Text(head.label)
+                                }
+                                //.param(w: width, h: 20)
                             }
-                            //.param(w: width, h: 20)
-                        }
-                        
-                    }.frame(alignment: .center)
-                }.frame(height:height)
-            }
-        } else {
-            if let head = head {
-                HStack {
-                    if ajout {
-                        TextField(prompt,text:Binding<String>(
+                            
+                        }.frame(alignment: .center)
+                    }.frame(height:height)
+                }
+            } else {
+                if let head = head {
+                    if input {
+                        if prompt != "" {Text(prompt)}
+                        TextField("", text:Binding<String>(
                             get: {head.label},
                             set:{self.head = Head("",$0)}
                         ))
-                        Button(action:{ajout = false})
+                        Button(action:{input = false})
                         {Image(systemName: "checkmark")}
-                        Button(action:{open = true})
+                        Button(action:{choice = true})
                         {Image(systemName: "magnifyingglass")}
                     } else {
                         Text(head.label)
-                        Button(action:{ ajout = true })
+                        Button(action:{ input = true })
                         {Image(systemName: "pencil")}
-                        
                     }
+                } else {
+                    Button(action:{head = Head("","") ;  input = true})
+                    { Text(head == nil ? table.name.singulier : head!.label) }
+                        .disabled(table.items.isEmpty)
                 }
-            } else {
-                Button(action:{open = true})
-                { Text(head == nil ? table.name.singulier : head!.label) }
-                    .disabled(table.items.isEmpty)
             }
+
         }
         
     }
     
     func choose(_ item:Head) {
         head = item
-        open = false
-        ajout = false
+        choice = false
+        input = false
     }
 }
 
 struct HeadPickerPreview : View {
    var table = tables["banques"]!
     @State var head: Head? = nil
-    @State var open = false
+    @State var choice = false
     
     var body: some View {
         VStack {
@@ -97,8 +102,8 @@ struct HeadPickerPreview : View {
                 .font(.title2)
                 .padding(20)
            
-            HeadPicker($head, table)
-                    .frame(width:200)
+            HeadPicker($head, table, "banque")
+                    .frame(width:250)
           
         }.padding(10)
     }
